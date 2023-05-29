2a5d9aaebd6c90f107bb8afc94392e157755e0ee
clemensgg <csc@soundportal.at>
2023-05-29 13:58:46 +0200

    add port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/metrics', (req, res) => {
   res.end(client.register.metrics());
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(config.port, () => console.log('Server listening on port 3000'));
 
 db.run('CREATE TABLE IF NOT EXISTS pending_packets (sequence INTEGER, timestamp TEXT, src_chain TEXT, src_channel TEXT, src_port TEXT, dst_chain TEXT)', (err) => {
   if (err) {
