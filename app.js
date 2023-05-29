07163a2d10d3c7b4c0e2b47d6e47dec547cf730b
clemensgg <csc@soundportal.at>
2023-05-29 14:17:46 +0200

    structure

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const cmd = require('node-cmd');
+const { exec } = require('child_process');
 const axios = require('axios');
 const moment = require('moment');
 const client = require('prom-client');
@@ -6,6 +6,8 @@ const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 
 const config = require('./config.json');
+
+const register = new client.Registry();
 
 const app = express();
 const db = new sqlite3.Database('./hermes_backlog.db');
@@ -17,10 +19,12 @@ const packetPendingGauge = new client.Gauge({
 
 app.get('/metrics', async (req, res) => {
   try {
+    console.log("Handling request for /metrics");
     const metrics = await register.metrics();
     res.set('Content-Type', register.contentType);
     res.end(metrics);
   } catch (err) {
+    console.error("Error while generating metrics:", err);
     res.status(500).end(err);
   }
 });
@@ -29,12 +33,15 @@ app.listen(config.port, () => console.log('Server listening on port ' + config.p
 
 db.run('CREATE TABLE IF NOT EXISTS pending_packets (sequence INTEGER, timestamp TEXT, src_chain TEXT, src_channel TEXT, src_port TEXT, dst_chain TEXT)', (err) => {
   if (err) {
-    return console.error(err.message);
+    console.error("Error while creating database:", err.message);
+    return;
   }
+  console.log("Database created successfully");
 });
 
 async function main() {
   try {
+    console.log("Starting main function");
     const configFilePath = config.configFilePath;
     const api = config.api;
   
@@ -67,14 +74,16 @@ async function main() {
         return chains;
       }, []);
 
-    console.log(chains);
-
-    setInterval(async () => {
+    console.log("Found chains:", JSON.stringify(chains));
+    
+    const runTask = async () => {
+      console.log("Running task");
       for (const chain of chains) {
         for (const channel of chain.channels) {
+          console.log(`Processing chain: ${chain.chain_id}, channel: ${channel.channel_id}`);
           const hermesCommand = `hermes --config ${configFilePath} query packet pending --chain ${chain.chain_id} --port ${channel.port_id} --channel ${channel.channel_id}`;
           const result = await new Promise((resolve, reject) => {
-            cmd.get(hermesCommand, (err, data) => {
+            exec(hermesCommand, (err, data) => {
               if (err) reject(err);
               else resolve(data);
             });
@@ -96,10 +105,12 @@ async function main() {
 
           db.all(`SELECT sequence FROM pending_packets WHERE src_chain = ? AND src_channel = ? AND src_port = ? AND dst_chain = ?`, [chain.chain_id, channel.channel_id, channel.port_id, channel.dst_chain_id], (err, rows) => {
             if (err) {
+              console.error("Database select error:", err);
               throw err;
             }
 
             const existingSequences = rows.map(row => row.sequence);
+            console.log("Existing sequences:", existingSequences);
 
             // add new sequences
             for (const sequence of sequences) {
@@ -107,8 +118,10 @@ async function main() {
                 db.run(`INSERT INTO pending_packets (sequence, timestamp, src_chain, src_channel, src_port, dst_chain) VALUES (?, ?, ?, ?, ?, ?)`,
                 [sequence, moment().format('YYYY-MM-DD HH:mm:ss'), chain.chain_id, channel.channel_id, channel.port_id, channel.dst_chain_id], (err) => {
                   if (err) {
+                    console.error("Database insert error:", err);
                     throw err;
                   }
+                  console.log("Inserted sequence:", sequence);
                 });
               }
             }
@@ -119,18 +132,27 @@ async function main() {
                 db.run(`DELETE FROM pending_packets WHERE sequence = ? AND src_chain = ? AND src_channel = ? AND src_port = ? AND dst_chain = ?`,
                 [existingSequence, chain.chain_id, channel.channel_id, channel.port_id, channel.dst_chain_id], (err) => {
                   if (err) {
+                    console.error("Database delete error:", err);
                     throw err;
                   }
                   packetPendingGauge.labels(chain.chain_id, channel.channel_id, channel.port_id, channel.dst_chain_id).dec();
+                  console.log("Decreased gauge for sequence:", existingSequence);
                 });
               }
             }
           });
         }
       }
-    }, 60000);
+    };
+
+    // Run the task immediately
+    await runTask();
+
+    // Run the task every minute
+    setInterval(runTask, 60000);
+ 
   } catch (error) {
-    console.error(error);
+    console.error("Error in main function:", error);
     process.exit(1);
   }
 }
